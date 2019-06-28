e610e826d18bf62fce8d3a482ddb5f9a052f4a23
Francois Pigerre <fpigerre@gmail.com>
2019-06-28 13:06:46 +1000

    Split instruction logic to Instruction.rs

---
+++ src/Instruction.rs
@@ -0,0 +1,40 @@
+use std::convert::TryFrom;
+
+const INCREMENT_POINTER: u8 = 0x3E;
+const DECREMENT_POINTER: u8 = 0x3C;
+const INCREMENT_VALUE: u8 = 0x2B;
+const DECREMENT_VALUE: u8 = 0x2D;
+const OUTPUT_VALUE: u8 = 0x2E;
+const ACCEPT_INPUT: u8 = 0x2C;
+const JUMP_FORWARD: u8 = 0x5B;
+const JUMP_BACKWARD: u8 = 0x5D;
+
+pub enum Instruction {
+    IncrementPointer = INCREMENT_POINTER as isize,
+    DecrementPointer = DECREMENT_POINTER as isize,
+    IncrementValue = INCREMENT_VALUE as isize,
+    DecrementValue = DECREMENT_VALUE as isize,
+    OutputValue = OUTPUT_VALUE as isize,
+    AcceptInput = ACCEPT_INPUT as isize,
+    JumpForward = JUMP_FORWARD as isize,
+    JumpBackward = JUMP_BACKWARD as isize
+}
+
+impl TryFrom<u8> for Instruction {
+    type Error = &'static str;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        use Instruction::*;
+        match value {
+            INCREMENT_POINTER => Ok(IncrementPointer),
+            DECREMENT_POINTER => Ok(DecrementPointer),
+            INCREMENT_VALUE => Ok(IncrementValue),
+            DECREMENT_VALUE => Ok(DecrementValue),
+            OUTPUT_VALUE => Ok(OutputValue),
+            ACCEPT_INPUT => Ok(AcceptInput),
+            JUMP_FORWARD => Ok(JumpForward),
+            JUMP_BACKWARD => Ok(JumpBackward),
+            _ => Err("Unrecognised instruction")
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate num;
-
 use std::io;
 
 use std::env;
@@ -9,29 +7,74 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
 
-use std::str;
+use std::convert::TryFrom;
 
-enum Instruction {
-    IncrementPointer = 0x3E,
-    DecrementPointer = 0x3C,
-    IncrementValue = 0x2B,
-    DecrementValue = 0x2D,
-    OutputValue = 0x2E,
-    AcceptInput = 0x2C,
-    JumpForward = 0x5B,
-    JumpBackward = 0x5D
+pub mod instruction;
+
+struct Program {
+    cells: Vec<u8>,
+    data_pointer: usize,
+    program_iterator: BufReader<File>
 }
 
-impl Instruction {
-    fn from_u8(value: u8) -> Option<Instruction> {
-        let instruction: Option<Instruction> = num::FromPrimitive::from_u8(value);
-        instruction
+// TODO: Yes, I know I'm forcing myself to use OOP for Rust
+// Yes, I will eventually redesign this and make it less OOP and more functional/data-oriented
+impl Program {
+    fn parse_instruction(&mut self, instruction: instruction::Instruction) {
+        use instruction::Instruction::*;
+        match instruction {
+            IncrementPointer => self.increment_pointer(),
+            DecrementPointer => self.decrement_pointer(),
+            IncrementValue => self.increment_value(),
+            DecrementValue => self.decrement_value(),
+            OutputValue => self.output_value(),
+            AcceptInput => self.accept_input(),
+            JumpForward => self.jump_forward(),
+            JumpBackward => self.jump_backward()
+        }
+    }
+
+    fn parse_sequence(self, sequence: Vec<instruction::Instruction>) {
+
+    }
+
+    fn increment_pointer(&mut self) {
+        self.data_pointer += 1;
+    }
+
+    fn decrement_pointer(&mut self) {
+        self.data_pointer -= 1;
+    }
+
+    fn increment_value(&mut self) {
+        self.cells[self.data_pointer] += 1;
+    }
+
+    fn decrement_value(&mut self) {
+        self.cells[self.data_pointer] -= 1;
+    }
+
+    fn output_value(&mut self) {
+        io::stdout().write(&[self.cells[self.data_pointer]]);
+    }
+
+    fn accept_input(&mut self) {
+        io::stdin().read(&mut [self.cells[self.data_pointer]]);
+    }
+
+    fn jump_forward(&mut self) {
+        // if the byte at the data pointer is zero,
+        // then instead of moving the instruction pointer forward to the next command,
+        // jump it forward to the command after the matching ] command.
+
+    }
+
+    fn jump_backward(&mut self) {
+        // if the byte at the data pointer is nonzero,
+        // then instead of moving the instruction pointer forward to the next command,
+        // jump it back to the command after the matching [ command.
     }
 }
-
-// Initialise 30, 000 8-bit cells at runtime
-static mut CELLS: Vec<u8> = Vec::with_capacity(30000);
-static mut DATA_POINTER: usize = 0;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -44,34 +87,28 @@ fn main() -> std::io::Result<()> {
     let path = Path::new(filename);
 
     let file = File::open(path)?; // ? Means push errors up the stack
-    let mut buf_reader = BufReader::new(file);
 
-    interpret_program(&mut buf_reader);
+    // Initialise 30, 000 8-bit cells at runtime
+    let cells: Vec<u8> = Vec::with_capacity(30000);
+    let data_pointer: usize = 0;
+    let program_iterator: BufReader<File> = BufReader::new(file);
+    let mut program = Program{  cells, data_pointer, program_iterator };
+
+    interpret_program(&mut program);
 
     Ok(())
 }
 
-fn interpret_program(buf_reader: &mut BufReader<File>) {
-    for byte in buf_reader.bytes() {
-        // For now we assume each character is in ASCII and is one byte long
-        let instruction : Option<Instruction> = Instruction::from_u8(byte.unwrap());
-        match instruction {
-            Some(valid_instruction) => interpret_instruction(valid_instruction),
-            None => panic!("Unrecognised instruction")
-        }
-    }
-}
+fn interpret_program(program: &mut Program) {
+    use instruction::Instruction;
 
-fn interpret_instruction(instruction: Instruction) {
-    use Instruction::*;
-    match instruction {
-        IncrementPointer => { data_pointer += 1 },
-        DecrementPointer => { data_pointer -= 1 },
-        IncrementValue => { cells[data_pointer] += 1 },
-        DecrementValue => { cells[data_pointer] -= 1 },
-        OutputValue => io::stdout().write(&cells[data_pointer]),
-        AcceptInput => io::stdin().read(&mut cells[data_pointer]),
-        JumpForward => {},
-        JumpBackward => {}
+    for byte in program.program_iterator.bytes() {
+        // For now we assume each character is in ASCII and is one byte long
+        let instruction : Instruction = match Instruction::try_from(byte.unwrap()) {
+            Ok(valid_instruction) => valid_instruction,
+            Err(e) => { println!("{}", e); continue; }
+        };
+
+        program.parse_instruction(instruction);
     }
 }
